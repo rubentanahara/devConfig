c94ab4440bd9c462e626b2f5b552c42dbe21f50f
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-09-21 20:03:34 -0600

    addin terminal to nvim
    super coool!

--- nvim/lua/tanaharaguy/core/highlights.lua
+++ nvim/lua/tanaharaguy/core/highlights.lua
@@ -1,12 +1,12 @@
 local opt = vim.opt
-vim.opt.winblend = 0
-vim.opt.wildoptions = 'pum'
-vim.opt.pumblend = 5
+opt.winblend = 0
+opt.wildoptions = "pum"
+opt.pumblend = 5
 
 -- highlight yanked text for 200ms using the "Visual" highlight group
-vim.cmd [[
+vim.cmd([[
   augroup highlight_yank
   autocmd!
   au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
   augroup END
-]]
+]])

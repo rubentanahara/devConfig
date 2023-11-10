520e804f78c23e2417710790b7d8afc310831d75
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-11-10 06:32:24 -0600

    update px to rem

---
+++ nvim/lua/plugins/px-to-rem.lua
@@ -0,0 +1,8 @@
+return {
+  "jsongerber/nvim-px-to-rem",
+  config = true,
+  --If you need to set some options replace the line above with:
+  -- config = function()
+  --     require('nvim-px-to-rem').setup()
+  -- end,
+}

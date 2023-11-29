77c5af3041931b2a0aeaa513860d6dffd82ef73a
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-11-29 16:16:02 -0600

    update change to nightfly

--- nvim/lua/plugins/github-theme.lua
+++ nvim/lua/plugins/github-theme.lua
@@ -3,7 +3,7 @@
 return {
   "projekt0n/github-nvim-theme",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  -- priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     require("github-theme").setup({
       options = {
@@ -15,6 +15,6 @@ return {
       },
     })
 
-    vim.cmd("colorscheme github_dark_tritanopia")
+    -- vim.cmd("colorscheme github_dark_tritanopia")
   end,
 }

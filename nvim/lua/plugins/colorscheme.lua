02271842eab0dbd183c06cb0d48a64378fcb2f86
Ruben Tanahara <72570761+rubentanahara@users.noreply.github.com>
2024-02-09 17:21:20 -0600

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,30 @@
 return {
+  -- add gruvbox
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load gruvbox
   {
-    "bluz71/vim-nightfly-guicolors",
-    priority = 1000,
-    config = function()
-      --load the colorscheme
-      vim.g.nightflyTransparent = 3
-      vim.cmd([[colorscheme nightfly]])
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
   },
 }
+
+-- return {
+--   "rebelot/kanagawa.nvim",
+--   priority = 1000,
+--   -- vim.cmd("colorscheme kanagawa"),
+-- }
+
+-- return {
+--   {
+--     "bluz71/vim-nightfly-guicolors",
+--     priority = 1000,
+--     config = function()
+--       --load the colorscheme
+--       -- vim.g.nightflyTransparent = 3
+--       -- vim.cmd([[colorscheme nightfly]])
+--     end,
+--   },
+-- }

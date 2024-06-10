3baee13607d34092dc383a26b3273591b4ebec73
Ruben Tanahara <tanaharaguy@gmail.com>
2024-06-10 09:32:18 -0600

    feat: update nvim config
    - Update various Neovim plugins to their latest versions.- Replace
    nightfly colorscheme with material.nvim in Neovim.- Adjust tmux
    statusline colors to match new Neovim colorscheme.- Add new plugins to
    Neovim: CopilotChat.nvim, lazydev.nvim,   material.nvim,
    mini.hipatterns, ts-comments.nvim.- Remove unused plugins from Neovim:
    LuaSnip, cmp_luasnip,   flutter-tools.nvim, github-nvim-theme,
    mini.comment,   nvim-ts-context-commentstring, nvim-vtsls,
    vim-nightfly-guicolors.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,73 @@
+return {
+  "marko-cerovac/material.nvim",
+  config = function()
+    require("material").setup({
+      contrast = {
+        sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+        floating_windows = true, -- Enable contrast for floating windows
+        line_numbers = true, -- Enable contrast background for line numbers
+        sign_column = true, -- Enable contrast background for the sign column
+        cursor_line = true, -- Enable darker background for the cursor line
+        non_current_windows = true, -- Enable darker background for non-current windows
+        popup_menu = true, -- Enable lighter background for the popup menu
+      },
+
+      styles = { -- Give comments style such as bold, italic, underline etc.
+        comments = { italic = true },
+        -- strings = { bold = true },
+        keywords = { underline = false },
+        functions = { italic = true, undercurl = false },
+        variables = {
+          italic = false,
+          undercurl = false,
+          bold = false,
+        },
+        operators = { bold = false },
+        types = { italic = false },
+      },
+
+      plugins = { -- Uncomment the plugins that you use to highlight them
+        "dap",
+        "gitsigns",
+        "indent-blankline",
+        "nvim-cmp",
+        "nvim-navic",
+        "nvim-tree",
+        "nvim-web-devicons",
+        "telescope",
+        "trouble",
+        "which-key",
+      },
+
+      -- disable = {
+      --   colored_cursor = false, -- Disable the colored cursor
+      --   borders = false, -- Disable borders between verticaly split windows
+      --   -- background = true, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
+      --   term_colors = false, -- Prevent the theme from setting terminal colors
+      --   eob_lines = false, -- Hide the end-of-buffer lines
+      -- },
+
+      high_visibility = {
+        lighter = false, -- Enable higher contrast text for lighter style
+        darker = true, -- Enable higher contrast text for darker style
+      },
+
+      lualine_style = "stealth", -- Lualine style ( can be 'stealth' or 'default' )
+
+      async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
+      custom_colors = nil, -- If you want to everride the default colors, set this to a function
+      custom_highlights = {}, -- Overwrite highlights with your own
+    })
+
+    -- Set the theme style
+    vim.g.material_style = "darker"
+
+    -- Load the colorscheme
+    vim.cmd([[colorscheme material]])
+  end,
+}
+
 -- return {
 --   { "ellisonleao/gruvbox.nvim" },
 --
@@ -29,17 +99,17 @@
 --   -- vim.cmd("colorscheme kanagawa"),
 -- }
 
-return {
-  {
-    "bluz71/vim-nightfly-guicolors",
-    priority = 1000,
-    config = function()
-      --load the colorscheme
-      vim.g.nightflyTransparent = 3
-      vim.cmd([[colorscheme nightfly]])
-    end,
-  },
-}
+-- return {
+--   {
+--     "bluz71/vim-nightfly-guicolors",
+--     priority = 1000,
+--     config = function()
+--       --load the colorscheme
+--       vim.g.nightflyTransparent = 3
+--       vim.cmd([[colorscheme nightfly]])
+--     end,
+--   },
+-- }
 --
 -- return {
 --   {

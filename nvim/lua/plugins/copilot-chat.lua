bb7968f14c5d9ef98cf3ae0969043c790cf7b5ff
Ruben Tanahara <tanaharaguy@gmail.com>
2024-06-11 08:34:23 -0600

    update: tmux status line

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -8,8 +8,22 @@ return {
     },
     opts = {
       debug = true, -- Enable debugging
-      -- See Configuration section for rest
+      -- Additional configuration for the best user experience
+      use_tabs = true, -- Use tabs for switching between different chats
+      auto_scroll = true, -- Automatically scroll to the latest message
+      chat_layout = {
+        position = "right", -- Position of the chat window (left, right, top, bottom)
+        size = 30, -- Size of the chat window
+      },
+      notifications = true, -- Enable notifications for new messages
     },
+    -- keys = {
+    --   -- Mapped keys for opening chats faster
+    --   { "n", "<Leader>cc", ":CopilotChat<CR>", { noremap = true, silent = true } }, -- Open main chat window
+    --   { "n", "<Leader>cn", ":CopilotChatNew<CR>", { noremap = true, silent = true } }, -- Start a new chat
+    --   { "n", "<Leader>cl", ":CopilotChatList<CR>", { noremap = true, silent = true } }, -- List all chats
+    --   { "n", "<Leader>cq", ":CopilotChatClose<CR>", { noremap = true, silent = true } }, -- Close the current chat
+    -- },
     -- See Commands section for default commands if you want to lazy load on them
   },
 }
